373046fd2ca572e2aa4e97913840f7b634394679
Michael Ding <yandy.ding@gmail.com>
2014-12-25 21:57:36 +0800

    add install fig

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ apt-get install -y docker.io
 echo 'export LC_ALL=C' >> ~/.bashrc
 echo '. /etc/bash_completion.d/docker.io' >> ~/.bashrc
 
+curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
+
