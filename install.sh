a6667289650745559cc03b518ac7b599469cccf9
Michael Ding <yandy.ding@gmail.com>
2014-12-27 01:10:17 +0800

    add more tools to init system

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y docker.io
+apt-get install -y docker.io python-pip libpython-dev curl
 
 echo 'export LC_ALL=C' >> ~/.bashrc
 echo '. /etc/bash_completion.d/docker.io' >> ~/.bashrc
 
 curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
-
