44658d3436cb7117167d13a0beb142f5eb90c5a5
Michael Ding <yandy.ding@gmail.com>
2015-04-26 16:32:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,3 +9,5 @@ wget -qO- https://get.docker.com/ | sh
 
 curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+ln -s /usr/local/bin/docker-compose /usr/local/bin/fig
