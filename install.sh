6b79dede11f481c9a636b37b98e645851233d278
Michael Ding <yandy.ding@gmail.com>
2015-04-26 16:23:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y docker.io python-pip libpython-dev curl
+apt-get install -y python-pip libpython-dev curl wget
 
 echo 'export LC_ALL=C' >> ~/.bashrc
-echo '. /etc/bash_completion.d/docker.io' >> ~/.bashrc
 
-curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
+wget -qO- https://get.docker.com/ | sh
+
+curl -L https://github.com/docker/compose/releases/download/1.2.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
